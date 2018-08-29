d751d9b825a914333909262cdefbb71eed82105a
AlexeyKarasev <karasev.alexey@gmail.com>
2018-08-29 22:44:42 +0300

    interim commit

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function () {
+  $('#task_language').on('change', function(e) {
+    const value = e.target.value;
+    const lang = langToAceLang[value];
+    
+  })
+});
+
+const langToAceLang = {
+  javascript: "javascript",
+  rust: "rust",
+  swift: "swift",
+  kotlin: "kotlin",
+  java: "java"
+}

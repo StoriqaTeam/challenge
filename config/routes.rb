90a67cb43ff11ba70164b424661b646eca789ded
Алексей Карасев <karasev.alexey@gmail.com>
2018-08-31 12:43:20 +0300

    added logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only: [:index, :show, :edit, :update, :destroy], path: :profiles
   devise_for :users, controllers: { omniauth_callbacks: 'users/oauth' }
-  root 'tasks#index'
+  root 'users#index'
 end

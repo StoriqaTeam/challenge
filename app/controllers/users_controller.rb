95221b336c814006f474099579c2f0021caae041
AlexeyKarasev <karasev.alexey@gmail.com>
2018-09-05 11:15:20 +0300

    added create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,29 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+ 
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    @user.password = SecureRandom.hex
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   # GET /users/1
   # GET /users/1.json
   def show
@@ -49,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:email, :role)
     end
 end

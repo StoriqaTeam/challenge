70d2e47f12cd6744889f9fe4abaab5c7c0ef4439
AlexeyKarasev <karasev.alexey@gmail.com>
2018-09-05 13:57:47 +0300

    added reference solution

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_065751) do
+ActiveRecord::Schema.define(version: 2018_09_05_105359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_09_04_065751) do
     t.text "initial"
     t.integer "time"
     t.text "initial_tests"
+    t.text "reference_solution"
   end
 
   create_table "users", force: :cascade do |t|
